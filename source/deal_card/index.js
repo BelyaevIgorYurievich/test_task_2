c7cc881a4c0b7e5107f0b2c4da7b7163e3e5f88b
Igor <justhiigor@yandex.ru>
2018-04-22 12:17:33 +0300

    Добавляет стили для отображения на мобильном телефоне

--- source/deal_card/index.js
+++ source/deal_card/index.js
@@ -33,8 +33,8 @@ class DealCard extends React.PureComponent {
 	cancelСreationUser = () => {
 		const {	setFieldValue } = this.props;
 		
-  	setFieldValue('isPersonalCardOpen', false);
-  } 
+  		setFieldValue('isPersonalCardOpen', false);
+  	} 
 
 	handleChengeValue = (type, value) => () => {
 		const { setFieldValue } = this.props;
@@ -77,96 +77,97 @@ class DealCard extends React.PureComponent {
 			 				/>
   	}
 
-    return (
-				<div className='static-modal personal-card '>
-					<Modal.Dialog>
-						<Modal.Header>
-							<Modal.Title>Создание сделки</Modal.Title>
-						</Modal.Header>
+    return(
+			<div className='static-modal personal-card '>
+				<Modal.Dialog>
+					<Modal.Header>
+						<Modal.Title>Создание сделки</Modal.Title>
+					</Modal.Header>
 
-						<Modal.Body>
-							<FormGroup>
-								<ControlLabel>Тип объекта сделки</ControlLabel>
-								<RadioButton 
-									isActive={isSelectTypeIsProduct} 
-									handleСhange={this.handleChengeValue('typeName', 'Товар')}
-									fieldName='Товар'
-								/>
-								<RadioButton 
-									isActive={!isSelectTypeIsProduct} 
-									handleСhange={this.handleChengeValue('typeName', 'Услуга')}
-									fieldName='Услуга'	
-								/>
-							</FormGroup>
-							<FormGroup>
-								<ControlLabel>Роль в сделке</ControlLabel>
-								<RadioButton 
-									isActive={isUserBuyer} 
-									handleСhange={this.handleChengeValue('role', isSelectTypeIsProduct ? 'Покупатель' : 'Заказчик')}
-									fieldName={isSelectTypeIsProduct ? 'Покупатель' : 'Заказчик'}
-								/>
-								<RadioButton 
-									isActive={isUserSeller} 
-									handleСhange={this.handleChengeValue('role', isSelectTypeIsProduct ? 'Продавец' : 'Исполнитель')}
-									fieldName={isSelectTypeIsProduct ? 'Продавец' : 'Исполнитель'}	
-								/>
-							</FormGroup>
-							<FormGroup>
-      							<ControlLabel>Вы будете участвовать в сделке как</ControlLabel>
-								<FormControl onChange={this.handleChengeValueWithEvent('nameId')} 
-									value={values.nameId} 
-									componentClass="select" 
-									placeholder="select">
-									<option value=''/>
-									{
-										values.userNames.map(({id, name, surname, patronymic}, index) => {
-											return <option key={id} value={id}>
-												{`${surname} ${name} ${patronymic}`}
-											</option>
-										})
-									}
-								</FormControl>
-							</FormGroup>
+					<Modal.Body>
+						<FormGroup>
+							<ControlLabel>Тип объекта сделки</ControlLabel>
+							<RadioButton 
+								isActive={isSelectTypeIsProduct} 
+								handleСhange={this.handleChengeValue('typeName', 'Товар')}
+								fieldName='Товар'
+							/>
+							<RadioButton 
+								isActive={!isSelectTypeIsProduct} 
+								handleСhange={this.handleChengeValue('typeName', 'Услуга')}
+								fieldName='Услуга'	
+							/>
+						</FormGroup>
+						<FormGroup>
+							<ControlLabel>Роль в сделке</ControlLabel>
+							<RadioButton 
+								isActive={isUserBuyer} 
+								handleСhange={this.handleChengeValue('role', isSelectTypeIsProduct ? 'Покупатель' : 'Заказчик')}
+								fieldName={isSelectTypeIsProduct ? 'Покупатель' : 'Заказчик'}
+							/>
+							<RadioButton 
+								isActive={isUserSeller} 
+								handleСhange={this.handleChengeValue('role', isSelectTypeIsProduct ? 'Продавец' : 'Исполнитель')}
+								fieldName={isSelectTypeIsProduct ? 'Продавец' : 'Исполнитель'}	
+							/>
+						</FormGroup>
+						<FormGroup>
+							<ControlLabel>Вы будете участвовать в сделке как</ControlLabel>
+							<FormControl onChange={this.handleChengeValueWithEvent('nameId')} 
+								value={values.nameId} 
+								componentClass="select" 
+								placeholder="select">
+								<option value=''/>
+								{
+									values.userNames.map(({id, name, surname, patronymic}, index) => {
+										return <option key={id} value={id}>
+											{`${surname} ${name} ${patronymic}`}
+										</option>
+									})
+								}
+							</FormControl>
+						</FormGroup>
+						<Button 
+							className='add-user-btn'
+							bsStyle='success'
+							disabled={isSubmitting}
+							onClick={this.handleOpenPersonal}
+						>Добавить пользователя</Button>
+						<FormGroup validationState={touched.productName && errors.productName ? 'error' : null}>
+							<ControlLabel>Название товара</ControlLabel>
+							<FormControl
+								type='text' 
+								placeholder='Введите название товара'
+								onChange={this.handleChengeValueWithEvent('productName')}
+								value={values.productName}
+							/>
+							{touched.productName && errors.productName &&<HelpBlock>{errors.productName}</HelpBlock>}
+						</FormGroup>
+						<FormGroup validationState={touched.description && errors.description ? 'error' : null}>
+							<ControlLabel>Описание товара</ControlLabel>
+							<FormControl
+								type='text' 
+								className='description'
+								componentClass='textarea'
+								placeholder='Введите oписание товара'
+								onChange={this.handleChengeValueWithEvent('description')}
+								value={values.description}
+							/>
+							{touched.description && errors.description &&<HelpBlock>{errors.description}</HelpBlock>}
+						</FormGroup>
+
+						</Modal.Body>
+						<Modal.Footer>
 							<Button 
-								className='add-user-btn'
-								bsStyle='success'
 								disabled={isSubmitting}
-								onClick={this.handleOpenPersonal}
-							>Добавить пользователя</Button>
-							<FormGroup validationState={touched.productName && errors.productName ? 'error' : null}>
-								<ControlLabel>Название товара</ControlLabel>
-								<FormControl
-									type='text' 
-									placeholder='Введите название товара'
-									onChange={this.handleChengeValueWithEvent('productName')}
-									value={values.productName}
-								/>
-								{touched.productName && errors.productName &&<HelpBlock>{errors.productName}</HelpBlock>}
-							</FormGroup>
-							<FormGroup validationState={touched.description && errors.description ? 'error' : null}>
-								<ControlLabel>Описание товара</ControlLabel>
-								<FormControl
-									type='text' 
-									componentClass="textarea"
-									placeholder='Введите oписание товара'
-									onChange={this.handleChengeValueWithEvent('description')}
-									value={values.description}
-								/>
-								{touched.description && errors.description &&<HelpBlock>{errors.description}</HelpBlock>}
-							</FormGroup>
+								bsStyle='primary'  
+								onClick={handleSubmit}
+							>Сохраить</Button>
+						</Modal.Footer>	
 
-							</Modal.Body>
-							<Modal.Footer>
-								<Button 
-									disabled={isSubmitting}
-									bsStyle='primary'  
-									onClick={handleSubmit}
-								>Сохраить</Button>
-							</Modal.Footer>	
-
-					</Modal.Dialog>
-				</div>
-    		)
+				</Modal.Dialog>
+			</div>
+    	   )
   	}
 }   
 
